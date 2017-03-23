aa3d3ed227ec80c6474716f25c1cac4206e57d7c
Mohamed Alami <contact@mohamed-alami.com>
2017-03-23 16:13:56 +0100

    Create var.php

---
+++ var.php
@@ -0,0 +1,165 @@
+<?php
+	$titre = "Bacchanight - Musée des Beaux Arts - Bordeaux";
+	machin@llll.fr
+	$email = "s.choux@mairie-bordeaux.fr";
+	$phone = "+33 5 56 10 20 56";
+	$street = ["20 Cours d'Albret","Galerie des Beaux-Arts<br/>Place du Colonel Raynal,"];
+	$zipcode = ["33000","33000"];
+	$city = ["Bordeaux","Bordeaux"];
+	$country = ["France","France"];
+	$address = ["<p>".$street[0]."<br>".$zipcode[0]." ".$city[0].", ".$country[0]."</p>","<p>".$street[1]."<br>".$zipcode[1]." ".$city[1].", ".$country[1]."</p>"];
+	$date = "MARDI 21 MARS 2017";
+	$heure = "20h - 00h";
+
+	// Programme
+	$programme = <<<XML
+<?xml version='1.0' standalone='yes'?>
+<ListeProgramme>
+	<programme>
+	  <event>
+			<location>M</location>
+	    <titre>20h15 : Défilé de mode</titre>
+	    <description>
+	      Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+	    </description>
+	  </event>
+	  <event>
+			<location>M</location>
+	    <titre>20h15 : Défilé de mode</titre>
+	    <description>
+	      Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+	    </description>
+	  </event>
+	  <event>
+			<location>M</location>
+	    <titre>20h15 : Défilé de mode</titre>
+	    <description>
+	      Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+	    </description>
+	  </event>
+	  <event>
+			<location>M</location>
+	    <titre>20h15 : Défilé de mode</titre>
+	    <description>
+	      Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+	    </description>
+	  </event>
+	  <event>
+			<location>M</location>
+	    <titre>20h15 : Défilé de mode</titre>
+	    <description>
+	      Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+	    </description>
+	  </event>
+	  <event>
+			<location>M</location>
+	    <titre>20h15 : Défilé de mode</titre>
+	    <description>
+	      Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+	    </description>
+	  </event>
+	  <event>
+			<location>M</location>
+	    <titre>20h15 : Défilé de mode</titre>
+	    <description>
+	      Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+	    </description>
+	  </event>
+	  <event>
+			<location>M</location>
+	    <titre>20h15 : Défilé de mode</titre>
+	    <description>
+	      Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+	    </description>
+	  </event>
+	  <event>
+			<location>M</location>
+	    <titre>20h15 : Défilé de mode</titre>
+	    <description>
+	      Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+	    </description>
+	  </event>
+	  <event>
+			<location>M</location>
+	    <titre>20h15 : Défilé de mode</titre>
+	    <description>
+	      Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+	    </description>
+	  </event>
+	  <event>
+			<location>M</location>
+	    <titre>20h15 : Défilé de mode</titre>
+	    <description>
+	      Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+	    </description>
+	  </event>
+	  <event>
+			<location>M</location>
+	    <titre>20h15 : Défilé de mode</titre>
+	    <description>
+	      Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+	    </description>
+	  </event>
+	</programme>
+	<programme>
+		<event>
+			<location>M</location>
+			<titre>20h15 : Défilé de mode</titre>
+			<description>
+				Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+			</description>
+		</event>
+		<event>
+			<location>M</location>
+			<titre>20h15 : Défilé de mode</titre>
+			<description>
+				Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+			</description>
+		</event>
+		<event>
+			<location>M</location>
+			<titre>20h15 : Défilé de mode</titre>
+			<description>
+				Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+			</description>
+		</event>
+		<event>
+			<location>M</location>
+			<titre>20h15 : Défilé de mode</titre>
+			<description>
+				Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+			</description>
+		</event>
+		<event>
+			<location>M</location>
+			<titre>20h15 : Défilé de mode</titre>
+			<description>
+				Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+			</description>
+		</event>
+		<event>
+			<location>M</location>
+			<titre>20h15 : Défilé de mode</titre>
+			<description>
+				Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+			</description>
+		</event>
+		<event>
+			<location>M</location>
+			<titre>20h15 : Défilé de mode</titre>
+			<description>
+				Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+			</description>
+		</event>
+		<event>
+			<location>M</location>
+			<titre>20h15 : Défilé de mode</titre>
+			<description>
+				Par les élèves de l’école de mode IBSM, coiffure et maquillage par les élèves de l’école Piger Création
+			</description>
+		</event>
+	</programme>
+</ListeProgramme>
+XML;
+
+?>
